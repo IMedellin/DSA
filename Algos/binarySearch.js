d6f6cc77c595c0ff592797202077f6a68fe536bb
Isaias <i.j.r.medellin@gmail.com>
2023-02-22 16:43:15 -0600

    binary search corrected

--- Algos/binarySearch.js
+++ Algos/binarySearch.js
@@ -10,31 +10,27 @@ const linearSearch = (array, element) =>{
     return `Not found Steps: ${steps}`
 }
 
-const orderedArray = [3, 17, 75, 87, 203, 245, 265, 266, 275, 309, 555]
+const orderedArray = [3, 17, 75, 87, 203, 245, 265, 266, 275, 309]
 const binarySearch = (array, element) =>{
-    let steps = 0;
     let start = 0;
     let end = array.length - 1;
-    while(start < end){
-        let mid = Math.floor((start + end) / 2);
-        if(array[mid] === element){
-            steps += 1;
-            return `${array[mid]} Binary Search Steps: ${steps}`; 
-        }else if(array[mid] > element){
-            steps += 1
-            end = mid;
-            mid = (start + end) / 2
-        }else if(array[mid] < element){
-            steps += 1;
-            start = mid; 
-            mid = (start + end) / 2;
+    while(start <= end){
+    let midPoint = Math.floor((start + end) / 2);
+    let middleValue = array[midPoint];
+        if(element === middleValue){
+            return middleValue;
+        }else if(element < middleValue){
+            end = midPoint - 1;
+        }else if(element > middleValue){
+            start = midPoint + 1;
         }
     }
+    return "Not found"
 }
 
 
 
 console.log(linearSearch(regArray, 75))
 console.log(linearSearch(regArray, 309))
-console.log(binarySearch(orderedArray, 309))
+console.log(binarySearch(orderedArray, 9))
 console.log(binarySearch(orderedArray, 75))
