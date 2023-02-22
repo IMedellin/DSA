8f6dedc177872b2621f695f263a007381224e695
Isaias Medellin <isaiasmedellin@Isaiass-MacBook-Air.local>
2023-02-22 11:14:07 -0600

    first commit

---
+++ binarySearch.js
@@ -0,0 +1,16 @@
+const regArray = [17, 3, 75, 202, 80];
+
+const linearSearch = (array, element) =>{
+    let steps = 0;
+    let result;
+    for(let i=0; i < array.length; i++){
+        steps++;
+        if(array[i] === element){
+            result = element;
+            return result + ` Steps: ${steps}`
+        }
+    }
+}
+
+console.log(linearSearch(regArray, 202))
+
