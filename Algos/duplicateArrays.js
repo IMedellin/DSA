195599104b87a6447a3546ac3f02050defe18419
Isaias Medellin <i.j.r.medellin@gmail.com>
2023-02-24 22:41:53 -0600

    duplicateValues in Array

---
+++ Algos/duplicateArrays.js
@@ -0,0 +1,28 @@
+const hasDuplicateValue = (array) => {
+  const valueIndexArray = []; //put an arbitrary value (1) at the index === to the value; if value is 3 place a (1) at index 3;
+
+  for (let i = 0; i < array.length; i++) {
+    if (valueIndexArray[array[i]] === 1) {
+      return "Has duplicate value";
+    } else {
+      valueIndexArray[array[i]] = 1;
+    }
+  }
+  return "No duplicate value";
+};
+
+const hasDuplicateValue2 = (array) => {
+  const checkDuplicateValue = {};
+  for (let i = 0; i < array.length; i++) {
+    let currentElement = array[i];
+    if (!checkDuplicateValue.hasOwnProperty(currentElement)) {
+      checkDuplicateValue[currentElement] = 1;
+    } else if (checkDuplicateValue.hasOwnProperty(currentElement)) {
+      return "Has duplicate value";
+    }
+  }
+  return "No duplicate value";
+}
+
+console.log(hasDuplicateValue([2, 4, 6, 8, 10, 12, 2]));
+console.log(hasDuplicateValue2([2, 4, 6, 8, 10, 12, 12]));
